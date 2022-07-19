0d73f3efbdcb4f2aa60bf596aab908987554ad47
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-07-19 14:45:44 +0200

    add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import AirportDetailPage from './pages/AirportDetailPage';
+import AuthPage from './pages/AuthPage';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
-    <div>hello</div>
+    <div>
+      <Routes>
+        <Route path="/" element={ <MainPage/>} />
+        <Route path="/auth" element={ <AuthPage/>} />
+        <Route path="/airport/:id" element={ <AirportDetailPage/>} />
+      </Routes>
+    </div>
   );
 }
 

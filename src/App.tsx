6af76225ca5e2bff139e161eb06cb426ecccaaec
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-07-19 14:56:10 +0200

    creer Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Navigation from './components/Navigation';
 import AirportDetailPage from './pages/AirportDetailPage';
 import AuthPage from './pages/AuthPage';
 import MainPage from './pages/MainPage';
 
 function App() {
   return (
-    <div>
+    <>
+      <Navigation/>
       <Routes>
         <Route path="/" element={ <MainPage/>} />
         <Route path="/auth" element={ <AuthPage/>} />
         <Route path="/airport/:id" element={ <AirportDetailPage/>} />
       </Routes>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navigation() {
+  return (
+    <nav className="flex justify-between px-5 h-[50px] bg-gray-200 items-center shadow-md"> 
+      Airport
+      <Link to="/auth">Auth</Link>
+    </nav>
+  );
+}
+
+export default Navigation;
